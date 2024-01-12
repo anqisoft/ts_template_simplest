fed5e4aaf969b5fa799a7e4432f4b9ba3f043f39
anqisoft <anqisoft@gmail.com>
2024-01-12 11:46:08 +0800

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+// deno-fmt-ignore-file
+// deno-lint-ignore-file
+// This code was bundled using `deno bundle` and it's not recommended to edit it manually
+
+const author = 'anqisoft@gmail.com';
+export { author as author };
